ea166fd3e6873769d811af4bed5e4630d500dba7
umatbro <umatbroo@gmail.com>
2018-02-17 10:53:00 +0100

    add test call

--- calc_test.py
+++ calc_test.py
@@ -6,3 +6,7 @@ class CalculatorTests(unittest.TestCase):
     def test_add(self):
         c = Calculator()
         self.assertEqual(c.add(1, 2), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
