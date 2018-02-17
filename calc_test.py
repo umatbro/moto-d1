655c62226f8d6cfe1d5befa87cfa3fae0b26369b
umatbro <umatbroo@gmail.com>
2018-02-17 10:43:24 +0100

    create calculator

---
+++ calc_test.py
@@ -0,0 +1,8 @@
+import unittest
+from calculator import Calculator
+
+
+class CalculatorTests(unittest.TestCase):
+    def test_add(self):
+        c = Calculator()
+        self.assertEqual(c.add(1, 2), 3)
---
+++ calculator.py
@@ -0,0 +1,6 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, value1, value2):
+        return value1 + value2
